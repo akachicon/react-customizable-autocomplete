2e8b1b64860f61f304ce8a34f66b2956b23ec23d
Zharenko <akachicon@gmail.com>
2020-09-30 14:00:33 +0300

    Remove examples from published files; bump version

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,6 @@ import replace from '@rollup/plugin-replace';
 import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 
-// TODO: write publishing script
-
 const isProd = process.env.NODE_ENV === 'production';
 
 const resolveOptions = {
