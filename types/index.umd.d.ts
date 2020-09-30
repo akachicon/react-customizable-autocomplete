b77a26e15c368da8e0c743ac6bf3d38623f71106
Zharenko <akachicon@gmail.com>
2020-09-30 12:52:57 +0300

    Change umd declaration name

--- types/index.umd.d.ts
+++ types/index.umd.d.ts
@@ -1,10 +1,8 @@
 import { AutocompleteProps } from './index';
 
-// TODO: check
-
-declare function Autocomplete<D = unknown>(
+declare function ReactCustomizableAutocomplete<D = unknown>(
   props: AutocompleteProps<D>
 ): JSX.Element;
 
-export as namespace Autocomplete;
-export = Autocomplete;
+export as namespace ReactCustomizableAutocomplete;
+export = ReactCustomizableAutocomplete;
